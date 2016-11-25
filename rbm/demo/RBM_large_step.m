278e9ae557c449b61d999e6f8cd94d202762a253
pp1336 <pp1336@users.noreply.github.com>
2016-11-25 13:35:14 +0000

    a

---
+++ rbm/demo/RBM_large_step.m
@@ -0,0 +1,16 @@
+hu = [800,900,1000];
+lr = (0.1);
+bs = [100,200];
+ep = (250);
+mo = (0);
+wp = (0);
+dp = (0);
+[bestError, error_matrix, bestR, bestH, bestL,...
+ bestB, bestE, bestM, bestW, bestD] = trainRBM(hu, lr, bs, ep, mo, wp, dp);
+
+fprintf('\nTerminated best error = %f\n', bestError);
+fprintf('\nbestH = %d, bestL = %f, bestB = %d, bestE = %d\n',...
+        bestH, bestL, bestB, bestE);
+
+save('bestR.mat', 'bestR');
+save('error_matrix.mat', 'error_matrix');
---
+++ rbm/demo/trainRBM.m
@@ -0,0 +1,83 @@
+function [bestError, error_matrix, bestR, bestH, bestL,...
+          bestB, bestE, bestM, bestW, bestD] = trainRBM(hu, lr, bs, ep, mo, wp, dp)
+
+fprintf('\nRBM large step (Caltech 101 Silhouette 28x28 datastet).\n');
+
+load('caltech101_silhouettes_28_split1.mat');
+
+[~,nVis] = size(train_data);
+
+bestError = inf;
+bestR = 0;
+bestH = 0;
+bestL = 0;
+bestB = 0;
+bestE = 0;
+error_matrix = zeros(length(hu),length(lr),length(bs),length(ep),...
+                     length(ep), length(ep), length(ep));
+
+t = 1;
+for m = mo
+u = 1;
+for w = wp
+v = 1;
+for d = dp
+w = 1;
+for h = hu
+    x = 1;
+    for l = lr
+        y = 1;
+        for b = bs
+            z = 1;
+            for e = ep
+
+                arch = struct('size', [nVis,h], 'classifier',true,...
+                              'inputType','binary');
+                arch.opts = {'verbose', 1, ...
+                         'lRate', l, ...
+                        'momentum', 0., ...
+                        'nEpoch', e, ...
+                        'wPenalty', 0., ...
+                        'dropout', 0,...
+                        'batchSz', b, ...
+                        'nGibbs', 1, ...
+                        };
+
+                r = rbm(arch);
+                r = r.train(train_data,single(train_labels));
+
+                [~,classErr,~] = r.classify(test_data,...
+                                                   single(test_labels));
+                error = classErr*100;
+                fprintf('\nCompleted with h = %d, l = %f, b = %d, e = %d\nerror = %f\n',...
+                        h, l, b, e, error);
+                error_matrix(w, x, y, z, t, u, v) = error;
+                
+                if error < bestError
+                    bestError = error;                    
+                    bestR = r;
+                    bestH = h;
+                    bestL = l;
+                    bestB = b;
+                    bestE = e;
+                    bestM = m;
+                    bestW = w;
+                    bestD = d;
+                end
+                
+                z = z + 1;
+            end
+            y = y + 1;
+        end
+        x = x + 1;
+    end
+    w = w + 1;
+    end
+    v = v + 1;
+    end
+    u = u + 1;
+    end
+    t = t + 1;
+end
+end
+
